d618f4d533c85a92267c2ed52b10196cccee8fc4
daPhie79 <33412188+daPhie79@users.noreply.github.com>
2018-03-15 20:45:30 -0400

    add source files, tools and hmods from old branch (could not rebase, it's a mess)

---
+++ SSH/SshClientException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace com.clusterrr.hakchi_gui.Ssh
+{
+    public class SshClientException : Exception
+    {
+        public SshClientException(string message) : base(message) { }
+    }
+}
